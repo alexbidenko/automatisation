fc3f7704d5f88b834aa1e2e7409599781d0fa468
Alexander <alexbidenko1998@gmail.com>
2020-05-31 20:44:47 +0300

    Create YTMusicAutocontinuer.js

---
+++ YTMusicAutocontinuer.js
@@ -0,0 +1,1 @@
+window.YTMusicAutocontinuer=setInterval(()=>{const a=document.querySelector('paper-dialog.style-scope.ytmusic-popup-container');if(a.style.display!=='none')document.querySelector('a.yt-simple-endpoint.style-scope.yt-button-renderer').click();},3000);
