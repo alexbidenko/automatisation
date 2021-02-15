db63532e60e9c87cd3175780892927cb944a4501
Alexander <alexbidenko1998@gmail.com>
2021-02-15 11:50:54 +0300

    Update server_setup.sh

--- server_setup.sh
+++ server_setup.sh
@@ -2,7 +2,7 @@
 
 apt-get remove docker docker-engine docker.io containerd runc
 apt-get update
-sudo apt-get install \
+apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -10,18 +10,18 @@ sudo apt-get install \
     software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
+apt-get install -y docker-ce docker-ce-cli containerd.io
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
-apt-get install htop
+apt-get install -y htop
 
 ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y 2>&1 >/dev/null
 cat ~/.ssh/id_rsa.pub > ~/.ssh/authorized_keys
